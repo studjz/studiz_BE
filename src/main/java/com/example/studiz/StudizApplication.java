c0b3faa159eab32605f8c04cf351d13666d1e1b9
seong-jun <seongjun154@naver.vom>
2026-02-09 09:50:33 +0900

    Initial commit

---
+++ src/main/java/com/example/studiz/StudizApplication.java
@@ -0,0 +1,13 @@
+package com.example.studiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studiz/StudizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
