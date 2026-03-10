54476f5f89924a3a146055e848c79aa9d61439f2
박성준 <seongjun154@naver.com>
2026-03-10 10:47:14 +0900

    db오류 해결

--- src/main/java/com/example/studiz/domain/userloadmap/repository/UserLoadMapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studiz.domain.userloadmap.repository;
-
-
-import com.example.studiz.domain.userloadmap.UserLoadMap;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLoadMapRepository extends JpaRepository<UserLoadMap, Integer> {
-
-}
